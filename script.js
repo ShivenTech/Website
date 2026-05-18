852ba2bfb544dc240aeab85601cf60ebdf743569
ShivenTech <shiven2018@gmail.com>
2026-05-19 00:09:48 +0800

    New ONe

---
+++ script.js
@@ -0,0 +1,4 @@
+// This function runs when user clicks the button
+function showMessage() {
+  alert("Thank you for your interest! Please contact Arban Gallery on Instagram @arbangallery to book a workshop.");
+}
